5035c9e829a240e70178a859a8513dca7d502cdf
adhada7 <adhada7@MD1M7UEC.ad001.siemens.net>
2018-07-17 15:19:32 +0200

    Fix input mask

--- js/admin.js
+++ js/admin.js
@@ -4,7 +4,9 @@
     $(function() {
       
 
-        $('.wcn_mask').inputmask({ regex: '-?[0-9]+.?([0-9]+)?(px|em|rem|ex|%|in|cm|mm|pt|pc)' });   
+        //$('.wcn_mask').inputmask({ regex: '-?[0-9]+.?([0-9]+)?(px|em|rem|ex|%|in|cm|mm|pt|pc)' }); 
+        $('.wcn_mask').inputmask({ regex: '-?[0-9]+([,.][0-9]+)?(px|em|rem|ex|%|in|cm|mm|pt|pc)' }); 
+        
         $('.wcn_font_select').fontselect().change(function(){
                 // replace + signs with spaces for css
                 var font = $(this).val().replace(/\+/g, ' ');
