4371683b457331c47bde426826bbaad54d0f91ca
adhada7 <adhada7@MD1M7UEC.ad001.siemens.net>
2018-10-14 21:59:45 +0200

    Refactor to class

--- js/adminWorkflowEditor.js
+++ js/adminWorkflowEditor.js
@@ -1,74 +1,72 @@
+class AdmninWorkflowEditor {
+  constructor() {
+    this.items = [];
+    $( '.sortable' ).sortable(({
+      update: function( event, ui ) {
+        renderAll();
+      },
+    }));
+    $( '.sortable' ).disableSelection();
 
- function allowDrop(ev) {
-    ev.preventDefault();
+    // pass options to plugin constructor
+    const s1 = new Sleep();
+    s1.selected(this.elementSelected);
+
+    this.addElement(s1);
+    $('.droparea').append(s1.getContent);
+
+    this.renderAll();
+
+    $('.draggable').draggable({
+      revert: 'invalid',
+      stack: '.draggable',
+      helper: 'clone',
+      cursor: 'move',
+      start: function(event, ui) {
+        $(this).draggable('instance').offset.click = {
+        left: Math.floor(ui.helper.width() / 2),
+        top: Math.floor(ui.helper.height() / 2),
+        };
+      },
+    });
+  };
+
+
+  renderAll() {
+    this.items.forEach(function(element) {
+      element.render();
+    });
+  }
+
+  addElement(el, render = false) {
+    this.items.push(el);
+    el.elementAdded(this.addElement.bind(this));
+    if (render == true) {
+      this.renderAll();
+    }
+  }
+
+  elementSelected(o) {
+    $('#editorarea').append(o.editor.getContent);
+  };
+
+  selectionCanged(element) {
+    $('#editorarea').append(element.getEditor());
+  }
 }
 
-function drag(ev,o) {
-    ev.dataTransfer.setData(o);
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev, o) {
+  ev.dataTransfer.setData(o);
 }
 
 function drop(ev) {
-    setTimeout(ShowPreviewPopup, 100)
+  setTimeout(ShowPreviewPopup, 100)
 }
-
-function selectionCanged(element)
-{
-    $("#editorarea").append(element.getEditor());
-}
-
 
 jQuery(document).ready(function($) {
-    var changed = false;
-    var items = [];
-
-    var renderAll = function()
-    {
-        items.forEach(function(element) {
-            element.render();
-        });
-    }
-
-    var addElement = function(el, render = false)
-    {
-        items.push(el);
-        el.elementAdded(addElement);
-        if(render == true)
-            renderAll();
-    }
-
-    var elementSelected = function(o)
-    {
-        $("#editorarea").append(o.editor.getContent);
-    };
-
-    $( ".sortable" ).sortable(({
-        update: function( event, ui ) {renderAll();}
-      }));
-    $( ".sortable" ).disableSelection();
-
-      // pass options to plugin constructor
-      var s1 = new Sleep();
-      s1.selected(elementSelected);
-
-      addElement(s1);
-      
-      $('.droparea').append(s1.getContent) 
-
-      renderAll();
-
-      $('.draggable').draggable({
-        revert: "invalid",
-        stack: ".draggable",
-        helper: 'clone',
-        cursor: "move",
-        
-        start: function(event, ui){
-            $(this).draggable('instance').offset.click = {
-            left: Math.floor(ui.helper.width() / 2),
-            top: Math.floor(ui.helper.height() / 2)
-        };} 
-      });
-
-     
-  
+  const awe = new AdmninWorkflowEditor();
 });
